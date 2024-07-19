2a60d1ac734fa9327fb8cfbca1a02f5861b6c37c
dtyronestrand <153030742+dtyronestrand@users.noreply.github.com>
2024-07-18 17:44:07 -0700

    inital commit

---
+++ system/user/config/config.php
@@ -0,0 +1,27 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['index_page'] = '';
+$config['site_license_key'] = '';
+$config['save_tmpl_files'] = 'y';
+// ExpressionEngine Config Items
+// Find more configs and overrides at
+// https://docs.expressionengine.com/latest/general/system-configuration-overrides.html
+
+$config['app_version'] = '7.4.11';
+$config['encryption_key'] = '17ce0f8fe13e66e370b3d415f7f09c516c4d491c';
+$config['session_crypt_key'] = '80d438a3eed45f17b63f7f98e1abd87d5906cde7';
+$config['database'] = array(
+	'expressionengine' => array(
+		'hostname' => 'sql106.byetcluster.com',
+		'database' => 'if0_36856805_nlmaf',
+		'username' => '36856805_1',
+		'password' => 'e).48OpSb8',
+		'dbprefix' => 'expmc_',
+		'char_set' => 'utf8mb4',
+		'dbcollat' => 'utf8mb4_unicode_ci',
+		'port'     => ''
+	),
+);
+$config['show_ee_news'] = 'y';
+
+// EOF
